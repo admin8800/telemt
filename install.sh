bb0f0aeaf4ea33cba1d1ed3e72da5c295f3f1abc
admin8800 <153331261+admin8800@users.noreply.github.com>
2026-06-10 08:03:41 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-REPO="${REPO:-telemt/telemt}"
+REPO="${REPO:-admin8800/telemt}"
 BIN_NAME="${BIN_NAME:-telemt}"
 INSTALL_DIR="${INSTALL_DIR:-/bin}"
 CONFIG_DIR="${CONFIG_DIR:-/etc/telemt}"
